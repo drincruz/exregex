8de890f9bab4c6cf952472aa753d0802b2955a21
Adrian Cruz <drincruz@gmail.com>
2019-05-13 23:29:18 -0400

    [Initial] Mix New
    Boiler Plate.

---
+++ lib/exregex.ex
@@ -0,0 +1,18 @@
+defmodule Exregex do
+  @moduledoc """
+  Documentation for Exregex.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Exregex.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/exregex_test.exs
@@ -0,0 +1,8 @@
+defmodule ExregexTest do
+  use ExUnit.Case
+  doctest Exregex
+
+  test "greets the world" do
+    assert Exregex.hello() == :world
+  end
+end
